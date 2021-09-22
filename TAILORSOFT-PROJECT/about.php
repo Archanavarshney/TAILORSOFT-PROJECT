0db95d30966053923204a284ca66e4451890de64
Archanavarshney <65590255+Archanavarshney@users.noreply.github.com>
2021-09-22 07:59:44 -0500

    Add files via upload

--- TAILORSOFT-PROJECT/about.php
+++ TAILORSOFT-PROJECT/about.php
@@ -1,1 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" name="viewport" content="width=device-width,initial-scale=1">
+	<title>Tailorsoft</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
+</style>
+</head>
+<body>
+	<!--navbar-->
+	<?php
+include 'header.php';
+  ?>
+	<!--end of navbar-->
+	<!--about-->
+
+      <center>
+<img src="img/30.jpg" alt="Mountain View" style="width:1200px;height:500px;padding-top: 100px">
+</center> 
+    
+<br />
+<div id="mid_content1">
+<div class="container1">
+<p> 
+<br /> We aim to provide automated, complete, and proven Performance Management Solutions to businesses of all sizes and industries and we back it up with industry leading service and support and our Best-Value Guarantee.
+
+At EPR Portal we believe that getting powerful performance management software should not be complicated and shouldn’t break the bank. We offer customers the tools needed to align, develop, reward, and retain a top-performing workforce without having to pay extra for each ‘module’ and without having to rely on IT or vendor support to maintain their system.
+
+</p>
+<!--<a href="services.jsp"> Click here</a> to view our Services of EPR Portal.-->
+<br />
+<br />
+<p>
+Established in 2004, our software suite was built by us from the ground up and was designed to be completely configurable by the end user, easy-to-use, and simple to maintain.
+Our 20+ years of software experience and our dedication to quality and customer satisfaction is what drives us to deliver powerful functionality, world-class customer training and support, and unparalleled value.
+
+We believe that EPR Portal provides the best and most cost effective platform for effectively administering, monitoring, and tracking performance, talent, compensation and succession across the enterprise.
+Since 1989, It has been a global provider of innovative business solutions that improve business performance and increase efficiency. It is a privately held company headquartered in India
+
+If you would like to learn more about EPR Portal, please contact us.</p></div>
+<br/>
+  <!--end of about-->
+  <!--footer-->
+  <?php
+  include 'footer.php';
+  ?>
+  <!-- END #footer -->
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>	
+<script src="https://use.fontawesome.com/b656b85fa0.js"></script>
+
+</body>
+</html>
