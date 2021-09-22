62457db4218bd977ef2c7423e9c5c3a65e493f79
Archanavarshney <65590255+Archanavarshney@users.noreply.github.com>
2021-09-22 08:01:38 -0500

    Add files via upload

---
+++ TAILORSOFT-PROJECT/fb/index.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" name="viewport" content="width=device-width,initial-scale=1">
+	<title></title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="style.css">
+ 
+</head>
+<body>
+
+  <div class="container-fluid">
+    <div class="row">
+       <div class="col-lg-12">
+         <div id="header">
+      <div id="logo">
+        <img src="1.jpg">
+      </div> 
+
+      <div id="form1">
+                <div id="email">
+                Email or phone
+                <input type="name/Email" name="Naman"><br>
+                Keep Me Log in
+                <input type="checkbox" name="">
+                </div>
+                <div id="password">
+                 Password
+                 <input type="password" placeholder="*********"><br>
+                  <a href="https://www.facebook.com/login/identify/?ctx=recover&ars=royal_blue_bar
+">Forgetten Account?</a>   
+                 </div>
+                 <div id="button">
+               
+               <button>Log In</button>
+                 </div>
+       </div>
+
+    
+  </div>
+       </div>
+    </div>
+  </div>
+
+  <section>
+           <div class="container-fluid">
+                <div class=" row mb-5">
+                     <div class="col-xl-10 col-lg-9 col-md-8 ml-auto">
+                        <div class="row align-items-center">
+                            <div class="col-xl-6 col-12 mb-4 mb-xl-0">
+                               <div class="content-block">
+                                  <div class="facebook-content">
+                                     <p style="font-size: 20px">Facebook helps you connect and share with the<br>
+                                      people in your life</p>
+                                     <img src="image2.png" >
+                                  </div>
+                                   <div class="create-account" style="margin-top: 150px;">
+                                     <h2>Create an account</h2>
+
+                                      <p>It's free and always will be.</p>
+                                   </div>
+                                </div>
+                            
+        <form  action="www.google.com" method="GET" style="float: right">
+        <div>
+            <label><input name="first name" type="name" id="name" size="10px"          placeholder="first name"></label>
+            <label><input  name="last name" type="name" placeholder="last name"></label>
+        </div>
+      <div style="margin:20px 0px" >
+        <input type="text"  placeholder="Email or Mobile number">
+      </div>
+      <div style="margin:20px 0px">
+        <input type="password"  placeholder="Re-enter email or mobile number ">
+      </div>
+      <div style="margin:20px 0px">
+        <input type="password"  placeholder="New password">
+      </div>
+      <div>
+        <label>Birthday:<br>
+              <select>
+                <option>Day</option>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+                <option>6</option>
+                <option>7</option>
+                <option>8</option>
+              </select>
+        </label>
+        <label>
+          <select>
+            <option>Month</option>
+            <option>jan</option>
+            <option>feb</option>
+            <option>Mar</option>
+            <option>Apr</option>
+            <option>May</option>
+            <option>Jun</option>
+            <option>Jul</option>
+            <option>Aug</option>
+          </select>
+        </label>
+        <label>
+          <select>
+            <option>Year</option>
+            <option>1999</option>
+            <option>2000</option>
+            <option>2001</option>
+            <option>2002</option>
+            <option>2003</option>
+            <option>2004</option>
+            <option>2005</option>
+            <option>2006</option>
+          </select>
+        </label>
+      </div>
+      <div id="gender">
+        Gender<br>
+        <label>Female<input id="Female" type="radio" name="gender"></label>
+        <label>Male<input id="Female" type="radio" name="gender"></label>
+        <label>Custom<input id="custom" type="radio" name="gender"></label>
+      </div>
+   </form>
+   <h4>By Clicking Sign Up you agree to <strong style="color: blue">Terms</strong> and that you have <br>read our <strong style="color: blue">Data policy</strong>,incluing your cookies Use</h4>
+
+    <input type="submit" value="Submit">
+      </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </section>
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>	
+<script src="https://use.fontawesome.com/b656b85fa0.js"></script>
+
+</body>
+</html>
